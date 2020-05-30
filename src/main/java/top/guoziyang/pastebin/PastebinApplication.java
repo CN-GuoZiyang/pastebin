4377a3320f68b8633d75873dc9e9922e130081ba
Ziyang <guoziyang0033@gmail.com>
2020-05-30 22:50:33 +0800

    init project

---
+++ src/main/java/top/guoziyang/pastebin/PastebinApplication.java
@@ -0,0 +1,13 @@
+package top.guoziyang.pastebin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastebinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastebinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/guoziyang/pastebin/PastebinApplicationTests.java
@@ -0,0 +1,13 @@
+package top.guoziyang.pastebin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastebinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
